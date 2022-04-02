35c7221d49f4b17313aaab1d8086e27c1c38d54f
VishnuV504 <97045581+VishnuV504@users.noreply.github.com>
2022-04-02 16:03:48 +0530

    Time: 1849 ms (5.00%), Space: 382.5 MB (5.00%) - LeetHub

--- 650-2-keys-keyboard/650-2-keys-keyboard.java
+++ 650-2-keys-keyboard/650-2-keys-keyboard.java
@@ -2,20 +2,24 @@ class Solution {
     public int minSteps(int n) {
         if(n==1)
             return 0;
-        return 1+fewSteps(1,n,1,false);  // 1+ is for because first time copy should be done 
+        int dp[][][]=new int[n][n][2];
+        return 1+fewSteps(1,n,1,0,dp);  // 1+ is for because first time copy should be done 
         
     }
-    private int fewSteps(int curr, int n,int prevCopied,boolean flag)
+    private int fewSteps(int curr, int n,int prevCopied,int flag,int dp[][][])
     {
         if(curr==n)
             return 0;
         if(curr>n)
             return 10000;
+        if(dp[curr][prevCopied][flag]!=0)
+            return dp[curr][prevCopied][flag];
         int copy=10000;
         int paste=10000;
-        if(flag==false)
-            copy=1+fewSteps(curr,n,curr,true);
-        paste = 1+fewSteps(curr+prevCopied,n,prevCopied,false);
-        return Math.min(copy,paste);
+        if(flag==0)
+            copy=1+fewSteps(curr,n,curr,1,dp);
+        paste = 1+fewSteps(curr+prevCopied,n,prevCopied,0,dp);
+        dp[curr][prevCopied][flag]=Math.min(copy,paste);
+        return dp[curr][prevCopied][flag];
     }
 }
