f3872e83879ea37b01a446c7c065808b68700c41
VishnuV504 <97045581+VishnuV504@users.noreply.github.com>
2022-04-04 11:50:25 +0530

    Time: 5 ms (6.21%), Space: 47.4 MB (5.14%) - LeetHub

---
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        // infinite transactions allowed so remove transaction count
+        int dp[][] = new int[2][prices.length+1];
+        for(int i=0;i<2;i++)
+                for(int l=0;l<=prices.length;l++)
+                    dp[i][l]=-10000;
+        return profit(0,prices,1,dp);
+        
+    }
+    private int profit(int curr,int prices[], int canBuy,int dp[][])
+    {
+        if(curr==prices.length)
+            return 0;
+        if(dp[canBuy][curr]!=-10000)
+            return dp[canBuy][curr];
+        int idle = profit(curr+1,prices,canBuy,dp);
+        int buy=0; int sell=0;
+        if(canBuy==1)
+            buy=-prices[curr]+profit(curr+1,prices,0,dp);
+        else
+            sell=prices[curr]+profit(curr+1,prices,1,dp);
+        dp[canBuy][curr]=Math.max(idle,Math.max(buy,sell));
+        return  dp[canBuy][curr];
+            
+        
+    }
+}
