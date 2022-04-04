8333a91e6a1a447daef1ed1ff9f888ce04d000da
VishnuV504 <97045581+VishnuV504@users.noreply.github.com>
2022-04-04 10:52:25 +0530

    Time: 109 ms (5.55%), Space: 118.5 MB (5.11%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.java
@@ -1,31 +1,30 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        Stack<Integer>st1 = new Stack<Integer>();
-        int profit=0;
-        int maxProfit=0;
-        for(int i=prices.length-1;i>=0;i--)
-        {
-            if(st1.empty())
-            {
-                profit=0;
-                st1.push(prices[i]);
-            }
-            else
-            {
-                if(st1.peek()>prices[i])
-                {
-                    profit= st1.peek()-prices[i];
-                }
-                else
-                {
-                    st1.push(prices[i]);
-                }
-            }
-            
-            if(profit>maxProfit)
-            maxProfit=profit;
-        }
-        return maxProfit;
+        int dp[][][] = new int[2][2][prices.length+1];
+        for(int i=0;i<2;i++)
+            for(int j=0;j<2;j++)
+                for(int k=0;k<=prices.length;k++)
+                    dp[i][j][k]=-100000;
+        return max(0,prices,1,1,dp);
         
+    }
+    private int max(int curr,int prices[],int flag,int k,int dp[][][])
+    {
+        if(curr==prices.length|| k==0)
+            return 0;
+        int buy=0;
+        int sell=0;
+        if(dp[k][flag][curr]!=-100000)
+            return dp[k][flag][curr];
+        int idle = max(curr+1,prices,flag,k,dp);
+        if(flag==1)
+            buy=-prices[curr]+max(curr+1,prices,0,k,dp);
+        else
+        {
+            sell = prices[curr]+max(curr+1,prices,1,k-1,dp);
+        }
+        dp[k][flag][curr]=Math.max(idle,Math.max(buy,sell));
+        return dp[k][flag][curr];
+            
     }
 }
