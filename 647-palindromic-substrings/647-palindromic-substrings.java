e3bb1e9e8ea7687ad2cfa1f1b51258fcc37375ed
VishnuV504 <97045581+VishnuV504@users.noreply.github.com>
2022-04-14 00:59:09 +0530

    Time: 708 ms (6.87%), Space: 46.4 MB (14.42%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.java
+++ 647-palindromic-substrings/647-palindromic-substrings.java
@@ -1,17 +1,21 @@
 class Solution {
     public int countSubstrings(String s) {
-        int count=0;
+        int count=0; int dp[][] = new int[s.length()+1][s.length()+1];
+        for(int i=0;i<=s.length();i++)
+            Arrays.fill(dp[i],-1);
         for(int i=0;i<s.length();i++)
-            count+=totalPalin(i,i,s);
+            count+=totalPalin(i,i,s,dp);
         return count;
     }
-    private int totalPalin(int index,int curr,String s)
+    private int totalPalin(int index,int curr,String s,int dp[][])
     {
         if(curr>=s.length())
             return 0;
+        if(dp[index][curr]!=-1)
+            return dp[index][curr];
         if(checkPalin(index,curr,s))
-            return 1+totalPalin(index,curr+1,s);
-        return totalPalin(index,curr+1,s);
+            return dp[index][curr]=1+totalPalin(index,curr+1,s,dp);
+        return dp[index][curr]=totalPalin(index,curr+1,s,dp);
     }
     private boolean checkPalin(int i,int j,String s)
     {
