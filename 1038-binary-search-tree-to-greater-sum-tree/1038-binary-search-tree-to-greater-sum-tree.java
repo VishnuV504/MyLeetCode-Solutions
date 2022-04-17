be8fac23ec7caf475043e59874c23b47844a8460
VishnuV504 <97045581+VishnuV504@users.noreply.github.com>
2022-04-17 12:41:11 +0530

    Time: 0 ms (100.00%), Space: 39.6 MB (93.77%) - LeetHub

---
+++ 1038-binary-search-tree-to-greater-sum-tree/1038-binary-search-tree-to-greater-sum-tree.java
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    List<Integer>li = new ArrayList<>();
+    HashMap<Integer,Integer>hs1 = new HashMap<>();
+    public TreeNode bstToGst(TreeNode root) {
+        traverse(root);
+        int prefixSum[] = new int[li.size()];
+        int prev=0;
+        for(int i=li.size()-1;i>=0;i--)
+        {
+            prefixSum[i]=prev+li.get(i);
+            hs1.put(li.get(i),prefixSum[i]);
+            prev=prefixSum[i];
+        }
+        modify(root);
+        return root;
+        
+    }
+    private void  traverse(TreeNode root)
+    {
+        if(root==null)
+            return ;
+        traverse(root.left);
+        li.add(root.val);
+        traverse(root.right);
+    }
+    private void modify(TreeNode root)
+    {
+        if(root==null)
+            return;
+        root.val=hs1.get(root.val);
+        modify(root.left);
+        modify(root.right);
+    }
+}
