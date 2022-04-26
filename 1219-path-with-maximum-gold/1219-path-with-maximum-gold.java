25dd9eab446f69628ffc27db49b8062439fc8af6
VishnuV504 <97045581+VishnuV504@users.noreply.github.com>
2022-04-26 10:17:02 +0530

    Time: 80 ms (75.51%), Space: 42.2 MB (24.76%) - LeetHub

--- 1219-path-with-maximum-gold/1219-path-with-maximum-gold.java
+++ 1219-path-with-maximum-gold/1219-path-with-maximum-gold.java
@@ -2,50 +2,27 @@ class Solution {
     public int getMaximumGold(int[][] grid) {
         int m= grid.length;
         int n=grid[0].length;
-        boolean visited[][] = new boolean[m][n];
-        int dp[][] = new int[m][n];
         int maxGold=0;
         for(int i=0;i<m;i++)
         {
             for(int j=0;j<n;j++)
             {
                 if(grid[i][j]!=0)
-                maxGold=Math.max(maxGold,solve(i,j,grid,dp,visited,m,n));   
+                maxGold=Math.max(maxGold,solve(i,j,grid,m,n));   
             }
         }
         return maxGold;  
     }
-    private int solve(int currRow,int currCol,int grid[][],int dp[][], boolean visited[][],int m, int n)
+    private int solve(int currRow,int currCol,int grid[][],int m, int n)
     {
-        if(currRow<0||currCol<0||currRow>=m||currCol>=n) return 0;
-        int up=0;int left=0;int right=0; int down=0;
-        if(grid[currRow][currCol]!=0&& visited[currRow][currCol]!=true)
-        {
-            if(currRow>=0)
-            {
-                visited[currRow][currCol]=true;
-                up=grid[currRow][currCol]+solve(currRow-1,currCol,grid,dp,visited,m,n);
-                visited[currRow][currCol]=false;
-            }
-            if(currRow<m)
-            {
-                visited[currRow][currCol]=true;
-                down=grid[currRow][currCol]+solve(currRow+1,currCol,grid,dp,visited,m,n);
-                visited[currRow][currCol]=false;
-            }
-            if(currCol>=0)
-            {
-                visited[currRow][currCol]=true;
-                left=grid[currRow][currCol]+solve(currRow,currCol-1,grid,dp,visited,m,n);
-                visited[currRow][currCol]=false;
-            }
-            if(currCol<n)
-            {
-                visited[currRow][currCol]=true;
-                right=grid[currRow][currCol]+solve(currRow,currCol+1,grid,dp,visited,m,n);
-                visited[currRow][currCol]=false;
-            }
-        }
+        if(currRow<0||currCol<0||currRow>=m||currCol>=n||grid[currRow][currCol]==0) return 0;
+        int temp=grid[currRow][currCol]; // storing the value of visited cell
+        grid[currRow][currCol]=0;//visited
+        int up=temp+solve(currRow-1,currCol,grid,m,n);
+        int down=temp+solve(currRow+1,currCol,grid,m,n);
+        int left=temp+solve(currRow,currCol-1,grid,m,n);
+        int right=temp+solve(currRow,currCol+1,grid,m,n);
+        grid[currRow][currCol]=temp; // backtrack
         int max=Math.max(left,Math.max(up,down));
         return Math.max(max,right);
     }
