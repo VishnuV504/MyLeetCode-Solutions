4cd46d55863650ad68d4ec8c9bcf9bdcca70df90
VishnuV504 <97045581+VishnuV504@users.noreply.github.com>
2022-04-27 18:23:10 +0530

    Time: 2 ms (93.52%), Space: 42.7 MB (97.20%) - LeetHub

--- 784-letter-case-permutation/784-letter-case-permutation.java
+++ 784-letter-case-permutation/784-letter-case-permutation.java
@@ -3,29 +3,24 @@ class Solution {
         List<String>ans = new ArrayList<String>();
         StringBuilder str = new StringBuilder(s);
         int length=s.length();
-        solve(0,s,str,ans,length,false);
+        solve(0,s,str,ans,length);
         return ans;
         
     }
-    private void solve(int curr,String s,StringBuilder str,List<String>ans,int length,boolean flag)
+    private void solve(int curr,String s,StringBuilder str,List<String>ans,int length)
     {
         if(curr>=length)
         {
             ans.add(str.toString());
             return;
         }
-        if(Character.isDigit(str.charAt(curr))) 
-        {
-            solve(curr+1,s,str,ans,length,false);
-            return;
-        }
-        if(flag==false)
+        if(!Character.isDigit(str.charAt(curr)))
         {
             str.setCharAt(curr,Character.toUpperCase(str.charAt(curr)));
-            solve(curr+1,s,str,ans,length,false);
+            solve(curr+1,s,str,ans,length);
             str.setCharAt(curr,Character.toLowerCase(str.charAt(curr)));
         }
-        solve(curr+1,s,str,ans,length,false);
+        solve(curr+1,s,str,ans,length);
         
     }
 }
