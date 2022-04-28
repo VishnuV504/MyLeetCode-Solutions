2606805760182804ee7358b29aec363f886e681a
VishnuV504 <97045581+VishnuV504@users.noreply.github.com>
2022-04-28 11:08:21 +0530

    Time: 32 ms (10.96%), Space: 193.2 MB (41.14%) - LeetHub

--- 131-palindrome-partitioning/131-palindrome-partitioning.java
+++ 131-palindrome-partitioning/131-palindrome-partitioning.java
@@ -2,11 +2,13 @@ class Solution {
     public List<List<String>> partition(String s) {
         List<List<String>>ans = new ArrayList<List<String>>();
         List<String>li = new ArrayList<>();
-        solve(0,s,ans,li);
+        HashMap<String,Boolean>hs1 = new HashMap<>();
+        generateAllPalin(s,hs1);
+        solve(0,s,ans,li,hs1);
         return ans;
         
     }
-    private void solve(int curr,String s,List<List<String>>ans,List<String>li)
+    private void solve(int curr,String s,List<List<String>>ans,List<String>li,HashMap<String,Boolean>hs1)
     {
         if(curr==s.length())
         {
@@ -17,10 +19,10 @@ class Solution {
         for(int i=curr;i<s.length();i++)
         {
             a+=s.charAt(i);
-            if(checkPalin(0,a.length()-1,a))
+            if(hs1.containsKey(a))
             {
                 li.add(a);
-                solve(i+1,s,ans,li);
+                solve(i+1,s,ans,li,hs1);
                 li.remove(li.size()-1);
             }
         }
@@ -36,5 +38,22 @@ class Solution {
             high--;
         }
         return true;
+    }
+    private void generateAllPalin(String s,HashMap<String,Boolean>hs1)
+    {
+        for(int i=0;i<s.length();i++)
+        {
+            String k="";
+            for(int j=i;j<s.length();j++)
+            {
+                k+=s.charAt(j);
+                if(!hs1.containsKey(k))
+                {
+                    if(checkPalin(0,k.length()-1,k))
+                        hs1.put(k,true);
+                }
+                
+            }
+        }
     }
 }
