6e57a52b8d923b2c9c1222e3cf27154029250f80
VishnuV504 <97045581+VishnuV504@users.noreply.github.com>
2022-05-01 13:56:28 +0530

    Time: 281 ms (5.04%), Space: 68.6 MB (7.02%) - LeetHub

---
+++ 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.java
@@ -0,0 +1,26 @@
+class Solution {
+    int maxLength=0;
+    int dir[][]= {{-1,0},{0,-1},{0,1},{1,0}};
+    public int longestIncreasingPath(int[][] matrix) {
+        int m=matrix.length;
+        int n= matrix[0].length;
+        HashMap<String,Integer>dp = new HashMap<>();
+        // for(int i=0;i<=m;i++)
+        //     Arrays.fill(dp[i],-1);
+        for(int i=0;i<m;i++)
+            for(int j=0;j<n;j++)
+                maxLength=Math.max(maxLength,solve(i,j,matrix,-1,m,n,dp));
+        return maxLength;    
+    }
+    private int solve(int i,int j, int matrix[][],int prev,int m,int n,HashMap<String,Integer>dp)
+    {
+        if(i<0||j<0||i>=m||j>=n||matrix[i][j]<=prev) return 0;
+        String currKey= i+"-"+j+"-"+prev;
+        if(dp.containsKey(currKey)) return dp.get(currKey);
+        int len=0;
+        for(int l=0;l<4;l++)
+        len=Math.max(len,1+solve(i+dir[l][0],j+dir[l][1],matrix,matrix[i][j],m,n,dp));
+        dp.put(currKey,len);
+        return len;
+    }
+}
