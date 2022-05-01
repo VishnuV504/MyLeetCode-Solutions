c87d02edc5f3c6e27a8809ca8e4a8a502e5bbb8e
VishnuV504 <97045581+VishnuV504@users.noreply.github.com>
2022-05-01 14:43:51 +0530

    Time: 13 ms (47.34%), Space: 54.3 MB (37.85%) - LeetHub

--- 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.java
+++ 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.java
@@ -4,7 +4,8 @@ class Solution {
     public int longestIncreasingPath(int[][] matrix) {
         int m=matrix.length;
         int n= matrix[0].length;
-        HashMap<String,Integer>dp = new HashMap<>();
+        //HashMap<String,Integer>dp = new HashMap<>();
+        int dp[][]= new int[m][n];
         // for(int i=0;i<=m;i++)
         //     Arrays.fill(dp[i],-1);
         for(int i=0;i<m;i++)
@@ -12,15 +13,17 @@ class Solution {
                 maxLength=Math.max(maxLength,solve(i,j,matrix,-1,m,n,dp));
         return maxLength;    
     }
-    private int solve(int i,int j, int matrix[][],int prev,int m,int n,HashMap<String,Integer>dp)
+    private int solve(int i,int j, int matrix[][],int prev,int m,int n,int[][] dp)
     {
         if(i<0||j<0||i>=m||j>=n||matrix[i][j]<=prev) return 0;
-        String currKey= i+"-"+j;
-        if(dp.containsKey(currKey)) return dp.get(currKey);
+       // String currKey= i+"-"+j;
+        //if(dp.containsKey(currKey)) return dp.get(currKey);
         int len=0;
+        if(dp[i][j]!=0) return dp[i][j];
         for(int l=0;l<4;l++)
         len=Math.max(len,1+solve(i+dir[l][0],j+dir[l][1],matrix,matrix[i][j],m,n,dp));
-        dp.put(currKey,len);
+        //dp.put(currKey,len);
+        dp[i][j]=len;
         return len;
     }
 }
