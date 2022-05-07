c39fd502d53400de4b6fd24e341f95804d4e5616
VishnuV504 <97045581+VishnuV504@users.noreply.github.com>
2022-05-07 13:09:34 +0530

    Time: 1 ms (100.00%), Space: 42.4 MB (80.37%) - LeetHub

--- 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.java
+++ 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.java
@@ -2,42 +2,25 @@ class Solution {
     public int minSubArrayLen(int target, int[] nums) {
         int n=nums.length;
         int minLength=Integer.MAX_VALUE;
-        int prefixSum[] = new int[n];
-        prefixSum[0]=nums[0];
-        if(nums[0]>=target) return 1;
-        for(int i=1;i<n;i++)
+        int j=0; int prev=0; int flag=0;
+        for(int i=0;i<n;i++)
         {
-            prefixSum[i]=prefixSum[i-1]+nums[i];
-            if(prefixSum[i]>=target)
+            while(prev+nums[i]>=target)
             {
-                int index=findUpperBound(prefixSum,prefixSum[i]-target,i-1);
-                //System.out.println(index+" "+i);
-               if(index!=-1)
-                   minLength=Math.min(minLength,i-index);
-                else
-                    minLength=Math.min(minLength,i+1);
+                prev-=nums[j];
+                j++;
+                flag=1;
+            }
+            prev+=nums[i];
+            if(flag==1)
+            {
+                flag=0;
+            minLength=Math.min(minLength,i-j+2);
             }
         }
-        if(minLength==Integer.MAX_VALUE)
-            return 0;
+        if(minLength==Integer.MAX_VALUE) return 0;
         return minLength;
-    }
-    private int findUpperBound(int prefixSum[],int target,int j)
-    {
-        int index=-1;
-        int i=0;
-        while(i<=j)
-        {
-            int mid=i+(j-i)/2;
-            if(prefixSum[mid]<=target)
-            {
-                index=mid;
-                i=mid+1;
-            }
-            else
-                j=mid-1;
-        }
-        return index;
+        
         
     }
 }
