b9d7098881dfdd9996d6300d93cccc0df8dc4040
VishnuV504 <97045581+VishnuV504@users.noreply.github.com>
2022-05-11 11:57:54 +0530

    Time: 1 ms (52.34%), Space: 43.6 MB (60.38%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.java
@@ -14,19 +14,19 @@
  * }
  */
 class Solution {
-    List<Integer>ans = new ArrayList<>();
+    long prev=Long.MIN_VALUE;
     public boolean isValidBST(TreeNode root) {
-        solve(root);
-        int n=ans.size();
-        for(int i=1;i<n;i++)
-            if(ans.get(i)<=ans.get(i-1)) return false;
-        return true;
+      return solve(root);
     }
-    private void solve(TreeNode root)
+    private boolean solve(TreeNode root)
     {
-        if(root==null) return;
-        solve(root.left);
-        ans.add(root.val);
-        solve(root.right);
+        if(root==null) return true;
+        boolean left=solve(root.left);
+        boolean mid=true;
+        if(root.val<=prev) mid=false;
+        else prev=root.val;
+        boolean right=solve(root.right);
+        return left&&right&&mid;
+        
     }
 }
