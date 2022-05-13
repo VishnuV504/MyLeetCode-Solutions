d203fddb9c235f14db3af72e8bc0163fff5927c8
VishnuV504 <97045581+VishnuV504@users.noreply.github.com>
2022-05-13 10:40:28 +0530

    Time: 1 ms (66.24%), Space: 47.3 MB (52.50%) - LeetHub

--- 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.java
+++ 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.java
@@ -23,26 +23,44 @@ class Node {
 
 class Solution {
     public Node connect(Node root) {
-                if(root==null) return root;
-        Queue<Node>que = new LinkedList<Node>();
-        que.add(root);
-        while(que.size()!=0)
-        {
-            int size=que.size();
-            while(size!=0)
-            {
-                Node curr=que.remove();
-                if(curr.left!=null)
-                    que.add(curr.left);
-                if(curr.right!=null)
-                    que.add(curr.right);
-                size--;
-                if(size!=0)
-                curr.next=que.peek();
-            }
-        }
-        return root;
-
         
+        /* using bfs*/
+        // if(root==null) return root;
+        // Queue<Node>que = new LinkedList<Node>();
+        // que.add(root);
+        // while(que.size()!=0)
+        // {
+        //     int size=que.size();
+        //     while(size!=0)
+        //     {
+        //         Node curr=que.remove();
+        //         if(curr.left!=null)
+        //             que.add(curr.left);
+        //         if(curr.right!=null)
+        //             que.add(curr.right);
+        //         size--;
+        //         if(size!=0)
+        //         curr.next=que.peek();
+        //     }
+        // }
+        // return root;
+        dfs(root,1);
+        return root;  
+    }
+    
+    // using dfs
+    List<Node>ans = new ArrayList<Node>();
+    private void dfs(Node root,int lvl)
+    {
+        if(root==null) return;
+        if(ans.size()<lvl)
+            ans.add(root);
+        else
+        {
+            ans.get(lvl-1).next=root;
+            ans.set(lvl-1,root);
+        }
+        dfs(root.left,lvl+1);
+        dfs(root.right,lvl+1);
     }
 }
