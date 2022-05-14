2bb99ff925c1687efd2d01426d38230fa98633d4
VishnuV504 <97045581+VishnuV504@users.noreply.github.com>
2022-05-14 16:04:59 +0530

    Time: 8 ms (64.99%), Space: 47.7 MB (28.51%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.java
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.java
@@ -8,37 +8,17 @@
  * }
  */
 class Solution {
-    List<TreeNode>pPath= new ArrayList<>();
-    List<TreeNode>qPath= new ArrayList<>();
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        solve(root,p,new ArrayList<TreeNode>(),1);
-        solve(root,q,new ArrayList<TreeNode>(),2);
-        TreeNode LCA=new TreeNode(-1); int j=0; int i=0;
-        while(i<pPath.size()&&j<qPath.size()){
-            if(pPath.get(i)==qPath.get(j))
-            {
-                LCA=pPath.get(i);
-                i++; j++;
-            }
-            else break;
-        }
-        return LCA;   
+        return solve(root,p,q);
     }
-    private void solve(TreeNode root,TreeNode val,List<TreeNode>path,int count)
+    private  TreeNode solve(TreeNode root,TreeNode p,TreeNode q)
     {
-        if(root==null) return;
-        if(root==val)
-        {
-            path.add(root);
-            if(count==1) 
-                for(int i=0;i<path.size();i++)pPath.add(path.get(i));
-            else
-                for(int i=0;i<path.size();i++)qPath.add(path.get(i));
-            return;
-        }
-        path.add(root);
-        solve(root.left,val,path,count);
-        solve(root.right,val,path,count);
-        path.remove(path.size()-1);
+        if(root==null) return null;
+        if(root==p) return p;
+        if(root==q) return q;
+        TreeNode left=solve(root.left,p,q);
+        TreeNode right=solve(root.right,p,q);
+        if(left!=null&&right!=null) return root;
+        return (left==null)?right:left;
     }
 }
