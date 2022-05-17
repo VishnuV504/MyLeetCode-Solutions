610ac3d55d53fea6766a0cb26574981d56696dcb
VishnuV504 <97045581+VishnuV504@users.noreply.github.com>
2022-05-17 06:22:25 +0530

    Time: 32 ms (5.96%), Space: 102.5 MB (30.82%) - LeetHub

--- 1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.java
+++ 1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.java
@@ -11,16 +11,35 @@
 class Solution {
     TreeNode ans = new TreeNode(-1);
     public final TreeNode getTargetCopy(final TreeNode original, final TreeNode cloned, final TreeNode target) {
-        solve(original,cloned,target);
-        return ans;
-        
+        // solve(original,cloned,target);
+        // return ans;
+        return solve2(original,cloned,target);    
     }
     private void solve(TreeNode original,TreeNode clones,TreeNode target)
     {
+        //recursive dfs
         if(original==null){return;}
         if(original==target){ans=clones; return;}
         solve(original.left,clones.left,target);
         solve(original.right,clones.right,target);
         return;
+    }
+    private TreeNode solve2(TreeNode original,TreeNode clone,TreeNode target)
+    {
+        //iterative dfs
+        Stack<Pair<TreeNode,TreeNode>>st1 = new Stack<Pair<TreeNode,TreeNode>>();
+        st1.push(new Pair<>(original,clone));
+        while(st1.size()!=0)
+        {
+            TreeNode curr1=st1.peek().getKey();
+            TreeNode curr2=st1.peek().getValue();
+            st1.pop();
+            if(curr1==target) return curr2;
+            if(curr1.right!=null)
+            st1.push(new Pair<>(curr1.right,curr2.right)); 
+            if(curr1.left!=null)
+            st1.push(new Pair<>(curr1.left,curr2.left));
+        }
+        return null;
     }
 }
