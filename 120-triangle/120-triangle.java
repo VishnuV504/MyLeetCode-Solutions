09dd10f8ebd2099fd4813f9927d984d5d55e7fea
VishnuV504 <97045581+VishnuV504@users.noreply.github.com>
2022-06-14 07:03:12 +0530

    Time: 11 ms (13.95%), Space: 43.6 MB (67.52%) - LeetHub

--- 120-triangle/120-triangle.java
+++ 120-triangle/120-triangle.java
@@ -1,29 +1,29 @@
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
-        List<List<Integer>>dp = new ArrayList<>();
+        List<Integer>dp = new ArrayList<>();
         List<Integer>li = new ArrayList<>();int OverAllMin=Integer.MAX_VALUE;
         for(int i=0;i<triangle.get(0).size();i++)
-        li.add(triangle.get(0).get(i));
-        dp.add(li); int m=triangle.size();
+        dp.add(triangle.get(0).get(i));
+        int m=triangle.size();
         for(int i=1;i<m;i++)
         {
-            List<Integer>li2 = new ArrayList<>();
+            List<Integer>li2 = new ArrayList<>(dp);
+            dp.clear();
             for(int j=0;j<triangle.get(i).size();j++)
             {
                 int up=Integer.MAX_VALUE;
                 int upL=Integer.MAX_VALUE;
-                if(dp.get(i-1).size()>j)
-                    up=dp.get(i-1).get(j);
-                if(j>=1&&dp.get(i-1).size()>j-1)
-                    upL=dp.get(i-1).get(j-1);
+                if(li2.size()>j)
+                    up=li2.get(j);
+                if(j>=1&&li2.size()>j-1)
+                    upL=li2.get(j-1);
                 int addToList=triangle.get(i).get(j)+Math.min(up,upL);
-                li2.add(addToList);  
+                dp.add(addToList);  
             }
-            dp.add(li2);
+            
         }
-        int n=dp.size()-1;
-        for(int j=0;j<dp.get(n).size();j++)
-            OverAllMin=Math.min(dp.get(n).get(j),OverAllMin); 
+        for(int j=0;j<dp.size();j++)
+            OverAllMin=Math.min(dp.get(j),OverAllMin); 
         return OverAllMin;
     }
 }
