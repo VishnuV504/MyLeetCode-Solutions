000790595f685a3ddfe6bf6b9f61d331ce0c406a
VishnuV504 <97045581+VishnuV504@users.noreply.github.com>
2022-06-28 07:15:57 +0530

    Time: 34 ms (31.34%), Space: 54 MB (71.51%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.java
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.java
@@ -1,28 +1,25 @@
 class Solution {
     public int minDeletions(String s) {
-        //sort by char freq
-        int n=s.length();
-        int arr[]= new int[26];
+       int n=s.length(); int freq[]= new int[26];  int count=0;
         for(int i=0;i<n;i++)
-            arr[(int)s.charAt(i)-97]++;
-        Arrays.sort(arr); int min=0;
-        HashMap<Integer,Boolean>hs1 = new HashMap<>();
-        for(int i=0;i<26;i++)
-        {
-            if(arr[i]!=0){
-                if(!hs1.containsKey(arr[i])) hs1.put(arr[i],true);
-                else {
-                    int j=arr[i];
-                    for(j=arr[i];j>0;j--){
-                        if(!hs1.containsKey(j)){
-                            hs1.put(j,true);
-                            break;
-                        }
-                    }
-                    min+=arr[i]-j;
+            freq[(int)(s.charAt(i)-'a')]++;
+        Arrays.sort(freq); HashMap<Integer,Integer>hs1 = new HashMap<>();
+        for(int i=25;i>=0;i--){
+            if(freq[i]==0)continue;
+            int j;
+            for(j=freq[i];j>=0;j--){
+                if(!hs1.containsKey(j)){
+                    hs1.put(j,1);
+                    break;
                 }
             }
+            System.out.println(j);
+            if(j>=0)
+            count+=freq[i]-j;
+            else
+                count+=freq[i];
         }
-        return min; 
+        return count;
+        
     }
 }
