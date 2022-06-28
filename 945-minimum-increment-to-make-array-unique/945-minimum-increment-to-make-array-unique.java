1c892b8e8e604a3e25aa7bc2c60924760de5d4fd
VishnuV504 <97045581+VishnuV504@users.noreply.github.com>
2022-06-28 08:10:22 +0530

    Time: 49 ms (59.05%), Space: 70.8 MB (61.83%) - LeetHub

--- 945-minimum-increment-to-make-array-unique/945-minimum-increment-to-make-array-unique.java
+++ 945-minimum-increment-to-make-array-unique/945-minimum-increment-to-make-array-unique.java
@@ -1,21 +1,17 @@
 class Solution {
     public int minIncrementForUnique(int[] nums) {
-        int n=nums.length; HashMap<Integer,Boolean>hs1= new HashMap<>();
+        int n=nums.length; 
         Arrays.sort(nums); int count=0; int prev=nums[0];
-        for(int i=0;i<n;i++){
+        for(int i=1;i<n;i++){
             int num=nums[i];
-            if(!hs1.containsKey(num)){
-                hs1.put(num,true);
+            if(nums[i]>prev)
                 prev=num;
-            }
-            else{
-                hs1.put(prev+1,true);
+            else{ 
                 count+=prev+1-nums[i];
                 prev++;
             }
         }
         return count;
-        //return solve(nums);
     }
     
 }
