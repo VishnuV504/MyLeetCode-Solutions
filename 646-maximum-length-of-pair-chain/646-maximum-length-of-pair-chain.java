65127e0b9e176facc2c5e73d78c859b6bcc62f7a
VishnuV504 <97045581+VishnuV504@users.noreply.github.com>
2022-07-07 16:52:51 +0530

    Time: 563 ms (5.10%), Space: 440.1 MB (6.39%) - LeetHub

--- 646-maximum-length-of-pair-chain/646-maximum-length-of-pair-chain.java
+++ 646-maximum-length-of-pair-chain/646-maximum-length-of-pair-chain.java
@@ -1,11 +1,11 @@
 class Solution {
     int dp[][]=new int[1001][2003];
     public int findLongestChain(int[][] pairs) {
-        Arrays.sort(pairs, (a, b) -> Integer.compare(a[0], b[0]));
-        for(int i=0;i<=1000;i++)
-            Arrays.fill(dp[i],-1);
-        return solve(0,-1001,pairs);
-        
+        //Arrays.sort(pairs, (a, b) -> Integer.compare(a[0], b[0]));
+        // for(int i=0;i<=1000;i++)
+        //     Arrays.fill(dp[i],-1);
+        // return solve(0,-1001,pairs);
+        return solve(pairs);
     }
     private int solve(int curr,int prev,int pairs[][]){
         if(curr==pairs.length)
@@ -16,5 +16,17 @@ class Solution {
             cons=1+solve(curr+1,pairs[curr][1],pairs);
         int notCons=solve(curr+1,prev,pairs);
         return dp[curr][prev+1001]=Math.max(cons,notCons);
+    }
+    private int solve(int pairs[][]){
+        Arrays.sort(pairs, (a, b) -> Integer.compare(a[1], b[1]));
+        int prev=-1001;
+        int maxLength=0;
+        for(int i=0;i<pairs.length;i++){
+            if(pairs[i][0]>prev){
+                maxLength++;
+                prev=pairs[i][1];
+            }
+        }
+        return maxLength;
     }
 }
