e138583fb5c431404f5364b7d2de203d65852033
VishnuV504 <97045581+VishnuV504@users.noreply.github.com>
2022-07-21 17:24:16 +0530

    Time: 318 ms (7.65%), Space: 49.8 MB (28.87%) - LeetHub

---
+++ 76-minimum-window-substring/76-minimum-window-substring.java
@@ -0,0 +1,37 @@
+class Solution {
+    public String minWindow(String s, String t) {
+        int freq[]=new int[128]; int index=-1;
+        for(int i=0;i<t.length();i++)
+            freq[(int)t.charAt(i)]++;
+        int freq2[]=new int[128]; int j=0; int minLength=Integer.MAX_VALUE;
+        for(int i=0;i<s.length();i++){
+            freq2[s.charAt(i)]++;
+            boolean flag=false;
+            while(isEqual(freq,freq2)){
+                System.out.println("me");
+                freq2[(int)s.charAt(j)]--;
+                j++;
+                flag=true;
+            }
+            if(flag){
+               // minLength=Math.min(minLength,i-j+1);
+                if(i-j+1<minLength){
+                    index=j;
+                    minLength=i-j+1;
+                }
+            }
+        }
+        String ans="";
+        if(index!=-1)
+            ans=s.substring(index-1,index+minLength);
+        return ans;
+        
+    }
+    public boolean isEqual(int freq[],int freq2[]){
+        for(int i=0;i<128;i++){
+            if(freq2[i]>=freq[i]) continue;
+            return false;
+        }
+        return true;
+    }
+}
